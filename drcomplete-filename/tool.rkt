34ac49128b34256ea8eda82770996f8587c9a30f
yjqww6 <343519265@qq.com>
2019-03-06 22:21:17 +0800

    better current directory rules (#3)
    In definition window, use the file path.
    In interaction window, be consistent with repl.

--- drcomplete-filename/tool.rkt
+++ drcomplete-filename/tool.rkt
@@ -4,15 +4,20 @@
 
 (require "private/main.rkt")
 
+(define-local-member-name get-dir)
+
 (define tool@
   (unit
     (import drracket:tool^)
     (export drracket:tool-exports^)
     (define phase1 void)
     (define phase2 void)
+
+    (define get-dir<%>
+      (interface () get-dir))
     
     (define fc-mixin
-      (mixin (racket:text<%> text:autocomplete<%>) ()
+      (mixin (racket:text<%> text:autocomplete<%>) (get-dir<%>)
         (inherit get-text get-backward-sexp get-start-position)
 
         (define/private (check-path pos)
@@ -35,16 +40,45 @@
           (cond
             [(check-path pos) => values]
             [else (super get-word-at pos)]))
+
+        (define/public (get-dir)
+          (current-directory))
         
         (define/override (get-all-words)
           (or
            (and-let*
             ([str (check-path (get-start-position))])
-            (get-completions str))
+            (parameterize ([current-directory (get-dir)])
+              (get-completions str)))
            (super get-all-words)))
         
         (super-new)
         ))
+
+    (define def-mixin
+      (mixin (drracket:unit:definitions-text<%> get-dir<%>) ()
+        (inherit get-tab)
+        (define/override (get-dir)
+          (or
+           (let ([t (get-tab)])
+             (send t get-directory))
+           (current-directory)))
+        (super-new)))
+
+    (define rep-mixin
+      (mixin (drracket:rep:text<%> get-dir<%>) ()
+        (inherit run-in-evaluation-thread)
+        
+        (define/override (get-dir)
+          (or dir (current-directory)))
+
+        (define dir #f)
+
+        (define/augment (after-many-evals)
+          (run-in-evaluation-thread (λ () (set! dir (current-directory))))
+          (inner #f after-many-evals))
+        
+        (super-new)))
     
-    (drracket:get/extend:extend-definitions-text fc-mixin)
-    (drracket:get/extend:extend-interactions-text fc-mixin)))
+    (drracket:get/extend:extend-definitions-text (compose def-mixin fc-mixin))
+    (drracket:get/extend:extend-interactions-text (compose rep-mixin fc-mixin))))
