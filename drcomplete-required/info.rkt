9a53634ff0cd6bca9309125a36cd5d9efa4e8499
yjqww6 <343519265@qq.com>
2019-03-06 10:42:33 +0800

    fix deps

--- drcomplete-required/info.rkt
+++ drcomplete-required/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "drcomplete-required")
-(define deps '("base" "drracket" "drracket-plugin-lib" "gui-lib" "srfi-lib"))
+(define deps '("base" "drracket" "drracket-plugin-lib" "gui-lib" "srfi-lib" "rackunit-lib"))
 (define build-deps '())
 (define pkg-desc "auto complete for required identifiers")
 (define version "0.1")
