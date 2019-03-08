17df072ed95cdca078a36e9f13037aeb698ce8bb
yjqww6 <343519265@qq.com>
2019-03-08 11:41:33 +0800

    drcomplete-module

---
+++ drcomplete-module/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+(define collection "drcomplete-module")
+(define deps '("base" "drracket" "drracket-plugin-lib" "gui-lib"))
+(define build-deps '())
+(define pkg-desc "auto complete for modules")
+(define version "0.1")
+(define pkg-authors '(yjqww6))
+
+
+(define drracket-tools '(("tool.rkt")))
+(define drracket-tool-names '("drcomplete-module"))
+(define drracket-tool-icons '(#f))
