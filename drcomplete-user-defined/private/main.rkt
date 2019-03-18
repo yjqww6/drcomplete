c39377df477b3cc49068d57680aacbec0d1daf96
yjqww6 <343519265@qq.com>
2019-03-18 15:28:18 +0800

    simplify

--- drcomplete-user-defined/private/main.rkt
+++ drcomplete-user-defined/private/main.rkt
@@ -26,50 +26,45 @@
     [_ (seteq)]))
 
 (define (walk stx [phase 0])
-  (define ret
-    (kernel-syntax-case/phase stx phase
-      [(#%expression ?expr) (walk #'?expr phase)]
-      [(#%plain-module-begin ?module-level-form ...)
-       (walk* #'(?module-level-form ...))]
-      [(module _ _ ?plain)
-       (walk* #'?plain 0)]
-      [(module* _ _ ?plain)
-       (walk* #'?plain 0)]
-      [(begin ?expr ...)
-       (walk* #'(?expr ...) phase)]
-      [(begin0 ?expr ...)
-       (walk* #'(?expr ...) phase)]
-      [(begin-for-syntax ?expr ...)
-       (walk* #'(?expr ...) (+ phase 1))]
-      [(define-values (?id ...) ?expr)
-       (set-union (visible #'(?id ...)) (walk #'?expr phase))]
-      [(define-syntaxes (?id ...) ?expr)
-       (set-union (visible #'(?id ...)) (walk #'?expr (+ phase 1)))]
-      [(#%plain-lambda ?formals ?expr ...)
-       (set-union (visible #'?formals) (walk* #'(?expr ...) phase))]
-      [(case-lambda (?formals ?expr ...) ...)
-       (set-union (visible #'(?formals ...)) (walk* #'(?expr ... ...) phase))]
-      [(if ?expr ...)
-       (walk* #'(?expr ...) phase)]
-      [(let-values ([(?id ...) ?expr] ...)
-         ?body ...)
-       (set-union (visible #'(?id ... ...))
-                  (walk* #'(?expr ... ?body ...) phase))]
-      [(letrec-values ([(?id ...) ?expr] ...)
-         ?body ...)
-       (set-union (visible #'(?id ... ...))
-                  (walk* #'(?expr ... ?body ...) phase))]
-      [(set! ?id ?expr)
-       (walk #'?expr phase)]
-      [(with-continuation-mark ?expr ...)
-       (walk* #'(?expr ...) phase)]
-      [(#%plain-app ?expr ...)
-       (walk* #'(?expr ...) phase)]
-      [_ (seteq)]))
-  
-  (cond
-    [(syntax-property stx 'disappeared-binding)
-     =>
-     (λ (ls)
-       (set-union (visible (datum->syntax #'k ls)) ret))]
-    [else ret]))
+  (set-union
+   (kernel-syntax-case/phase
+    stx phase
+    [(#%expression ?expr) (walk #'?expr phase)]
+    [(#%plain-module-begin ?module-level-form ...)
+     (walk* #'(?module-level-form ...) phase)]
+    [(module _ _ ?plain)
+     (walk* #'?plain 0)]
+    [(module* _ _ ?plain)
+     (walk* #'?plain 0)]
+    [(begin ?expr ...)
+     (walk* #'(?expr ...) phase)]
+    [(begin0 ?expr ...)
+     (walk* #'(?expr ...) phase)]
+    [(begin-for-syntax ?expr ...)
+     (walk* #'(?expr ...) (+ phase 1))]
+    [(define-values (?id ...) ?expr)
+     (set-union (visible #'(?id ...)) (walk #'?expr phase))]
+    [(define-syntaxes (?id ...) ?expr)
+     (set-union (visible #'(?id ...)) (walk #'?expr (+ phase 1)))]
+    [(#%plain-lambda ?formals ?expr ...)
+     (set-union (visible #'?formals) (walk* #'(?expr ...) phase))]
+    [(case-lambda (?formals ?expr ...) ...)
+     (set-union (visible #'(?formals ...)) (walk* #'(?expr ... ...) phase))]
+    [(if ?expr ...)
+     (walk* #'(?expr ...) phase)]
+    [(let-values ([(?id ...) ?expr] ...)
+       ?body ...)
+     (set-union (visible #'(?id ... ...))
+                (walk* #'(?expr ... ?body ...) phase))]
+    [(letrec-values ([(?id ...) ?expr] ...)
+       ?body ...)
+     (set-union (visible #'(?id ... ...))
+                (walk* #'(?expr ... ?body ...) phase))]
+    [(set! ?id ?expr)
+     (walk #'?expr phase)]
+    [(with-continuation-mark ?expr ...)
+     (walk* #'(?expr ...) phase)]
+    [(#%plain-app ?expr ...)
+     (walk* #'(?expr ...) phase)]
+    [_ (seteq)])
+   (visible (syntax-property stx 'disappeared-binding))))
